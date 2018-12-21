5ea910f74816ae3f0c3828f6d3bd7a28d52c283d
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2018-12-21 20:44:43 +0530

    Added Bottom Navigation Drawer

---
+++ app/src/main/java/android/gaurav/com/arcs19/BottomNavigationFragment.java
@@ -0,0 +1,53 @@
+package android.gaurav.com.arcs19;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.NavigationView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomNavigationFragment extends BottomSheetDialogFragment {
+
+    NavigationView navigationView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.bottom_navigation_fragment,container,false);
+
+        navigationView = rootView.findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int menuId = menuItem.getGroupId();
+
+                switch(menuId)
+                {
+                    case R.id.workshops : break;
+                    case R.id.hackathon : break;
+                    case R.id.convoke : break;
+                    case R.id.team : break;
+                }
+
+                return true;
+            }
+        });
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+
+    }
+}
