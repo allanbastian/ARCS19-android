12f151eeb583498a8bb5f89cd6e2d83293b9f5f8
Koushik Saha <Koushiksaha02@ymail.com>
2019-01-01 01:39:02 +0530

    Added Sponsor Fragment

--- app/src/main/java/android/gaurav/com/arcs19/SponsorAdapter.java
+++ app/src/main/java/android/gaurav/com/arcs19/SponsorAdapter.java
@@ -1,10 +1,6 @@
 package android.gaurav.com.arcs19;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,19 +15,17 @@ import java.util.ArrayList;
 public class SponsorAdapter extends RecyclerView.Adapter<SponsorAdapter.CustomViewHolder> {
 
     private ArrayList<DiscreteScrollClass> dataList;
-    private Context context;
-    public SponsorAdapter(Context context,ArrayList<DiscreteScrollClass> dataList){
-        this.context = context;
+
+    public SponsorAdapter(ArrayList<DiscreteScrollClass> dataList){
+
         this.dataList = dataList;
     }
 
     class CustomViewHolder extends RecyclerView.ViewHolder {
-        private CardView sponsor;
         private ImageView image;
         private TextView name;
         public CustomViewHolder(View itemView) {
             super(itemView);
-            sponsor = itemView.findViewById(R.id.sponsor_root_layout);
             name = itemView.findViewById(R.id.sponsor_name);
             image = itemView.findViewById(R.id.sponsor_image);
 
@@ -42,24 +36,14 @@ public class SponsorAdapter extends RecyclerView.Adapter<SponsorAdapter.CustomVi
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.sponsor_item_view, parent, false);
+        View view = layoutInflater.inflate(R.layout.item_sponsor_view, parent, false);
         return new CustomViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final SponsorAdapter.CustomViewHolder holder, final int i) {
+    public void onBindViewHolder(@NonNull final SponsorAdapter.CustomViewHolder holder, int i) {
         Glide.with(holder.itemView.getContext()).load(dataList.get(i).getIcon()).into(holder.image);
         holder.name.setText(dataList.get(i).getName());
-        holder.sponsor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(dataList.get(i).getDes()));
-                context.startActivity(intent);
-            }
-        });
     }
 
     @Override
