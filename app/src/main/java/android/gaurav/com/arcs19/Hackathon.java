88877ec784140dcaa79c79eabb9d1918ebee9a26
Siddharth <siddharthrajesh.2017@vitstudent.ac.in>
2019-01-10 00:51:17 +0530

    Some UI Changes

---
+++ app/src/main/java/android/gaurav/com/arcs19/Hackathon.java
@@ -0,0 +1,13 @@
+package android.gaurav.com.arcs19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hackathon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hackathon);
+    }
+}
