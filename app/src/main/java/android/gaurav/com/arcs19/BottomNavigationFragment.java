36d58d62442e71153de18a0ac7365697eed35296
Koushik Saha <Koushiksaha02@ymail.com>
2019-01-29 01:02:34 +0530

    Rebased on new login

--- app/src/main/java/android/gaurav/com/arcs19/BottomNavigationFragment.java
+++ app/src/main/java/android/gaurav/com/arcs19/BottomNavigationFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,13 +28,16 @@ public class BottomNavigationFragment extends BottomSheetDialogFragment {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                int menuId = menuItem.getGroupId();
+                int menuId = menuItem.getItemId();
 
                 switch(menuId)
                 {
                     case R.id.workshops : break;
                     case R.id.hackathon : break;
-                    case R.id.convoke : break;
+                    case R.id.convoke : Fragment fragment = new ConvokeFragment();
+                        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment_container, fragment).commit();
+                                        break;
                     case R.id.team : break;
                 }
                 getDialog().cancel();
