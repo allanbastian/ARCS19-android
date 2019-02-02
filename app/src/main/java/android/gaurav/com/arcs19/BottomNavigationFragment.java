7060c7b581e18336d10d9b6d86d6d9328d025ed8
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-02-03 00:48:10 +0530

    Added schedule

--- app/src/main/java/android/gaurav/com/arcs19/BottomNavigationFragment.java
+++ app/src/main/java/android/gaurav/com/arcs19/BottomNavigationFragment.java
@@ -1,10 +1,13 @@
 package android.gaurav.com.arcs19;
 
+import android.gaurav.com.arcs19.Schedule.SchedulePage;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,6 +37,10 @@ public class BottomNavigationFragment extends BottomSheetDialogFragment {
                     case R.id.hackathon : break;
                     case R.id.convoke : break;
                     case R.id.team : break;
+                    case R.id.schedule : Fragment fragment = new SchedulePage();
+                        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment_container, fragment).commit();
+                        break;
                 }
                 getDialog().cancel();
                 return true;
