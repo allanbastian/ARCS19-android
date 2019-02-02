cb9572450033796fdeff99a2d8cda17b75a984d9
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-02-03 00:52:43 +0530

    Added schedule

---
+++ app/src/main/java/android/gaurav/com/arcs19/Schedule/SchedulePage.java
@@ -0,0 +1,35 @@
+package android.gaurav.com.arcs19.Schedule;
+
+import android.gaurav.com.arcs19.R;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+public class SchedulePage extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    SchedulePagerAdapter schedulePagerAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.schedule_page,container,false);
+
+        tabLayout = rootView.findViewById(R.id.tab_bar);
+        viewPager = rootView.findViewById(R.id.view_pager);
+
+        schedulePagerAdapter = new SchedulePagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(schedulePagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+}
