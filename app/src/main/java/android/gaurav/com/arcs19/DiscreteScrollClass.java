2019ac5e357c735865245bf447105c191e831207
{Ragingvenomicity} <{a.ganguly1998@gmail.com}>
2019-02-02 22:48:02 +0530

    workshop updated

--- app/src/main/java/android/gaurav/com/arcs19/DiscreteScrollClass.java
+++ app/src/main/java/android/gaurav/com/arcs19/DiscreteScrollClass.java
@@ -2,16 +2,21 @@ package android.gaurav.com.arcs19;
 
 public class DiscreteScrollClass {
 
-    int icon;
-    String name;
-    String des;
+    int icon,price,authimg;
+    String name,sname;
+    String des,loc,authdesc;
 
     public DiscreteScrollClass(){}
 
-    public DiscreteScrollClass(int icon, String name, String des) {
+    public DiscreteScrollClass(int icon, String name, String des,String loc,String authdesc,int price,int authimg,String sname) {
         this.icon = icon;
         this.name = name;
         this.des = des;
+        this.loc=loc;
+        this.authdesc=authdesc;
+        this.price=price;
+        this.authimg=authimg;
+        this.sname=sname;
     }
 
     public int getIcon() {
@@ -38,4 +43,14 @@ public class DiscreteScrollClass {
         this.des = des;
     }
 
+    public String getLocation()
+    {return loc;}
+    public String getAuthDesc()
+    {return authdesc;}
+    public int getPrice()
+    {return price;}
+    public int getAuthImg()
+    {return authimg;}
+    public String getAuthName()
+    {return sname;}
 }
