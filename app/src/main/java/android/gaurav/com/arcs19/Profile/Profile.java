bed22f778b76357e19d53b6a5651e9151cc6bbf8
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-02-08 23:43:51 +0530

    Added Sign Out functionality

--- app/src/main/java/android/gaurav/com/arcs19/Profile/Profile.java
+++ app/src/main/java/android/gaurav/com/arcs19/Profile/Profile.java
@@ -1,13 +1,23 @@
 package android.gaurav.com.arcs19.Profile;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.gaurav.com.arcs19.Login.LoginActivity;
+import android.gaurav.com.arcs19.MainActivity;
 import android.gaurav.com.arcs19.R;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.yarolegovich.discretescrollview.DiscreteScrollView;
 import com.yarolegovich.discretescrollview.transform.ScaleTransformer;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -16,15 +26,47 @@ public class Profile extends AppCompatActivity {
     DiscreteScrollView scrollView;
     ArrayList<ProfileScrollClass> items;
     ProfileScrollAdapter adapter;
+    ImageButton qrButton, backButton;
+    FloatingActionButton signOutButton;
+
+    String USERNAME = "";
+    String EMAIL = "";
+    SharedPreferences sp;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        ImageButton imageButton = findViewById(R.id.qr_code_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        qrButton = findViewById(R.id.qr_code_button);
+        backButton = findViewById(R.id.back_button);
+        signOutButton = findViewById(R.id.sign_out_button);
+
+
+        sp = getSharedPreferences("key",0);
+
+        USERNAME = sp.getString("email","");
+
+
+        qrButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
 
             }
         });
@@ -49,6 +91,16 @@ public class Profile extends AppCompatActivity {
     }
 
     private void onItemChanged(ProfileScrollClass profileScrollClass) {
+    }
+
+    private void Logout()
+    {
+        SharedPreferences.Editor editor= sp.edit();
+        editor.remove("username").commit();
+        editor.remove("email").commit();
+        editor.remove("password").commit();
+        editor.remove("loginStatus").commit();
+        startActivity(new Intent(Profile.this, LoginActivity.class));
     }
 
 }
