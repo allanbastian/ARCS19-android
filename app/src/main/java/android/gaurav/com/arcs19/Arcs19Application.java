c91321baa392c3c1a81f5b6f9569e1bfeba1f49f
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-02-12 18:27:48 +0530

    Firebase Integration

---
+++ app/src/main/java/android/gaurav/com/arcs19/Arcs19Application.java
@@ -0,0 +1,14 @@
+package android.gaurav.com.arcs19;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Arcs19Application extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);         //Firebase caching
+    }
+}
